88f8184236d8c27d141e07fe0d6d146484648d5e
TalhaKocc <talhakocc1834@gmail.com>
2025-11-28 02:45:15 +0300

    admin kullanıcısı için öğretmen işlemleri tamamlandı

--- src/com/hospitalproject/controller/DoctorBean.java
+++ src/com/hospitalproject/controller/DoctorBean.java
@@ -7,6 +7,7 @@ import javax.faces.bean.SessionScoped;
 
 import com.hospitalproject.dto.ListAllDoctorDto;
 import com.hospitalproject.repository.DoctorDao;
+import com.hospitalproject.service.DoctorService;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,10 +16,29 @@ import lombok.Setter;
 @SessionScoped
 @Getter @Setter
 public class DoctorBean {
+	private Integer doctorId;
+	private Double salary;
 	
 	private DoctorDao doctorDao = new DoctorDao();
+	private DoctorService doctorService = new DoctorService(); 
 	
 	public List<ListAllDoctorDto> getListDoctorForAdmin() {
 		return doctorDao.listAllDoctor();
+	}
+
+	public String deleteDoctor(Integer id) {
+		doctorService.deleteDoctor(id);
+		return "admin-doctor-list?faces-redirect=true";
+	}
+
+	public String goToSalaryPage(Integer doctorId) {
+		this.doctorId=doctorId;
+		return "admin-doctor-salary-update.xhtml?faces-redirect=true";
+	}
+	
+	public String updateDoctorSalary() {
+		doctorService.updateDoctorSalary(doctorId, salary);
+		
+		return "admin-doctor-list.xhtml?faces-redirect=true";
 	}
 }
